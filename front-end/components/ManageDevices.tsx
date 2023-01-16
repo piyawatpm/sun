b32dca776d2bc257803662bb926aeb2be6f69a43
piyawatpm <piyawat_00101@hotmail.com>
2023-01-17 10:38:08 +1100

    fix 2 client in one groip bug

--- front-end/components/ManageDevices.tsx
+++ front-end/components/ManageDevices.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
   Accordion,
   AccordionHeader,
   AccordionBody,
-} from '@material-tailwind/react';
-import DeviceCards from './DeviceCards';
-import DeviceDashboard from './DeviceDashboard';
-import axios from 'axios';
-import { api } from '../lib/axios';
-import useSWR from 'swr';
+} from "@material-tailwind/react";
+import DeviceCards from "./DeviceCards";
+import DeviceDashboard from "./DeviceDashboard";
+import axios from "axios";
+import { api } from "../lib/axios";
+import useSWR from "swr";
 enum MenuState {
   DeviceDashboard,
   RentalManagement,
@@ -48,7 +48,7 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
         width="33"
         height="25"
         viewBox="0 0 33 25"
-        className={`${id === open ? 'rotate-180' : ''}  transition-transform`}
+        className={`${id === open ? "rotate-180" : ""}  transition-transform`}
       >
         <g
           id="Group_865"
@@ -66,13 +66,17 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
       </svg>
     );
   }
-
+  useEffect(() => {
+    console.log(clients);
+    console.log(selectedGroup.client);
+    console.log(deviceData);
+  }, []);
   return (
     <Accordion
       open={open}
       icon={<Icon2 id={1} open={open} />}
       className={`  absolute w-[1574px] top-0 ${
-        open && 'mt-[-8%]'
+        open && "mt-[-8%]"
       }  3xl:mt-[2%] translate-x-[-50%]   left-1/2 z-10  scale-[61%] 3xl:scale-100  flex flex-col`}
     >
       <AccordionHeader
@@ -87,12 +91,17 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
       </AccordionHeader>
 
       <AccordionBody className=" flex justify-center items-center w-full  space-x-[10px] py-[28px] bg-[#F5F5F5] h-[888px]  rounded-b-[10px] ">
-        <div className=" w-[332px] h-full flex flex-col space-y-10">
+        <div
+          onClick={() => {
+            console.log(deviceData);
+          }}
+          className=" w-[332px] h-full flex flex-col space-y-10"
+        >
           <div className=" flex flex-col py-3 w-full pl-3 text-[16px] font-bold bg-[#F5F5F5] rounded-[6px] shadow-md border-t-[3px] border-l-[3px]  border-white shadow-gray-300">
             <div className=" flex space-x-2">
               <p>CLIENT NAME:</p>
               <p className="text-[#636363]">
-                {clients?.find((e) => e.id == selectedGroup.client).name}
+                {/* {clients?.find((e) => e.id == selectedGroup.client).name} */}
               </p>
             </div>
             <div className=" flex space-x-2">
@@ -108,7 +117,7 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
             <div
               onClick={() => ChangeMenuTo(MenuState.DeviceDashboard)}
               className={`${
-                menuState !== MenuState.DeviceDashboard && ' opacity-20'
+                menuState !== MenuState.DeviceDashboard && " opacity-20"
               } text-[24px]  font-bold bg-[#F5F5F5] cursor-pointer text-[#636363] w-[275px] h-[151px] shadow-md   border-white shadow-gray-300 flex items-center justify-center rounded-[6px]`}
             >
               <p className=" max-w-[138px] text-center">Device Dashboard</p>
@@ -116,7 +125,7 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
             <div
               onClick={() => ChangeMenuTo(MenuState.RentalManagement)}
               className={`${
-                menuState !== MenuState.RentalManagement && ' opacity-20'
+                menuState !== MenuState.RentalManagement && " opacity-20"
               } text-[24px]  font-bold bg-[#F5F5F5] cursor-pointer text-[#636363] w-[275px] h-[151px] shadow-md   border-white shadow-gray-300 flex items-center justify-center rounded-[6px]`}
             >
               <p className=" max-w-[138px] text-center"> Rental Management</p>
@@ -124,7 +133,7 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
             <div
               onClick={() => ChangeMenuTo(MenuState.DeviceManager)}
               className={`${
-                menuState !== MenuState.DeviceManager && ' opacity-20'
+                menuState !== MenuState.DeviceManager && " opacity-20"
               } text-[24px]  font-bold bg-[#F5F5F5] cursor-pointer text-[#636363] w-[275px] h-[151px] shadow-md   border-white shadow-gray-300 flex items-center justify-center rounded-[6px]`}
             >
               <p className=" max-w-[138px] text-center"> Device Management</p>
@@ -132,7 +141,7 @@ const ManageDevices = ({ closeDeviceManage, selectedGroup }) => {
             <div
               onClick={() => ChangeMenuTo(MenuState.SimLog)}
               className={`${
-                menuState !== MenuState.SimLog && ' opacity-20'
+                menuState !== MenuState.SimLog && " opacity-20"
               } text-[24px]  font-bold bg-[#F5F5F5] cursor-pointer text-[#636363] w-[275px] h-[151px] shadow-md   border-white shadow-gray-300 flex items-center justify-center rounded-[6px]`}
             >
               <p className=" max-w-[138px] text-center"> Sim Log</p>
